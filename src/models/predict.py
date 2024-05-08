6a7118eb7057f4f6a24a2379c213714739c8efe1
AshishC17 <ashishchoudhary1717@gmail.com>
2024-05-08 18:20:43 +0530

    changes done

---
+++ src/models/predict.py
@@ -0,0 +1,30 @@
+import joblib
+import argparse
+import pandas as pd
+
+
+classifier_loaded = joblib.load("saved_models/01.knn_with_iris_dataset.pkl")
+encoder_loaded = joblib.load("saved_models/02.iris_label_encoder.pkl")
+
+
+# src/predict.py
+
+def main(input_file):
+    #df = pd.read_csv("/data/iris.csv")
+    df = pd.read_csv(input_file) 
+    classifier_loaded = joblib.load("output/iris_KNN_model.pkl")
+    encoder_loaded = joblib.load("output/iris_encoder.pkl") # Read input data
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1]
+    y = encoder_loaded.transform(y)
+    X = X.sample(10)
+    y = y.sample(10)
+
+    prediction_raw = classifier_loaded.predict(X)         # Get predictions
+    pd.DataFrame(prediction_raw).to_csv("output_predictions.csv", index=False)  # Save predictions
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run predictions with the model.")
+    parser.add_argument("input_csv", help="Path to the input CSV file")
+    args = parser.parse_args()
+    main(args.input_csv)
